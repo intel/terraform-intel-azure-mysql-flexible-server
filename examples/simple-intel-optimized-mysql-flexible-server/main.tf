7d987a50eceac795186382f383e6963c9c93a90f
lucasmelogithub <lucas.melo@intel.com>
2022-11-28 15:30:47 -0600

    Add simple example, fix Readme and refactor variables

---
+++ examples/simple-intel-optimized-mysql-flexible-server/main.tf
@@ -0,0 +1,14 @@
+# Provision Intel Optimized Azure MySql server
+module "optimized-mysql-server" {
+  source              = "../../"
+  resource_group_name = "terraform-testing-rg"   
+  db_server_name      = "terraformtestingpoc01" 
+  db_password         = var.db_password
+  db_firewall_rules = [
+    {
+      name             = "Services"
+      start_ip_address = "192.168.10.0"
+      end_ip_address   = "192.168.10.50"
+    }
+  ]  
+}
